e2e1cc7b4c04a5565218a686d7f3378aa1e0fae8
rtrichler <rbtrichler@ip-128-239-129-63.v4.wm.edu>
2016-02-19 14:30:44 -0500

    output to word table

--- KFWPoints_panelResults_Max_Weighted.R
+++ KFWPoints_panelResults_Max_Weighted.R
@@ -112,6 +112,16 @@ ggplot(data = psm_Long_TO, aes(x=Year, y=MaxL_,group=reu_id, colour=factor(UF)))
   stat_summary(fun.y=mean,aes(x=Year, y=MaxL_,group=reu_id,colour=factor(UF)),data=psm_Long_TO,geom='line',size=1.5)+
   theme(axis.text.x=element_text(angle=90,hjust=1))
 
+#------------------------------------------------------------------------
+#Workspace
+#------------------------------------------------------------------------
+
+dta_Shp@data$MaxL_diff <- dta_Shp@data$MaxL_2001-dta_Shp@data$MaxL_2000
+summary(dta_Shp@data$MaxL_diff)
+sd(dta_Shp@data$MaxL_diff)
+
+summary(dta_Shp@data$MaxL_2000)
+sd(dta_Shp@data$MaxL_2000)
 
 #------------------------------------------------------------------------
 #------------------------------------------------------------------------
@@ -125,7 +135,19 @@ stargazer(pModelMax_A_fit $cmreg,pModelMax_B_fit $cmreg,pModelMax_C_fit $cmreg,p
           dep.var.labels=c("Max NDVI")
 )
 
-stargazer(CMREG_A,CMREG_B,CMREG_C,CMREG_D,CMREG_E,
+library(R2HTML)
+library(stargazer)
+
+red = runif(100,0.0,1.0)
+green = runif(100,0.0,1.0)
+blue = runif(100,0.0,1.0)
+
+tDF <- data.frame(red, green, blue)
+
+testModel <- lm(red~blue + green, data=tDF)
+testModel2 <- lm(blue ~ green + red, data=tDF)
+
+table_1<-stargazer(CMREG_A,CMREG_B,CMREG_C,CMREG_D,CMREG_E,
           type="html",align=TRUE,keep=c("TrtMnt","MeanT_","MeanP_","Pop_","MaxT_","MaxP_","MinT_","MinP_","Year"),
           #covariate.labels=c("TrtMnt_demend_y","MeanT","MeanP","Pop","MaxT","MaxP","MinT","MinP","Year"),
           omit.stat=c("f","ser"),
@@ -133,5 +155,25 @@ stargazer(CMREG_A,CMREG_B,CMREG_C,CMREG_D,CMREG_E,
           dep.var.labels=c("Max NDVI")
 )
 
+#Put the directory and name you want.  Make sure the name ends
+#in ".doc".
+wordFile=file.path("/Users/rbtrichler/Documents/AidData/KFW Brazil Eval/KFW_Points","wordFile.docx")
+#Don't ever change this.
+cat("<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'>
+<head><title>Microsoft Office HTML Example</title></head>
+<body>", file=wordFile)
+
+#Add any tables you want from stargazer.
+cat(table_1, append=TRUE, file=wordFile)
+
+#You can also add normal text and line splits if you want, i.e.:
+text <- "The below table provides information on the BGR model, which has NULL results!:"
+cat(text,  append=TRUE, file=testdoc)
+
+
+#And, I can add the other table:
+#cat(table_2, append=TRUE, file=testdoc)
+
+cat("\n</body></html>", append=TRUE, file=wordFile)
 
 
